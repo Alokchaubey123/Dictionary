a8050107f0ca1ad4461076cc1e805f49c698c905
Alokchaubey123 <alokchaubey116@gmail.com>
2026-07-27 20:47:46 +0530

    Added sugestions giver

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 file = open("environmental_thesaurus_40k.json", "r", encoding="utf-8")
 data = json.load(file)
@@ -10,6 +11,14 @@ def search(word):
 
     if word in data:
         return data[word], word
+
+    guess = get_close_matches(word, data.keys(), 1, 0.8)
+
+    if len(guess) > 0:
+        ans = input("Did you mean " + guess[0] + "? (Y/N): ")
+
+        if ans.lower() == "y":
+            return data[guess[0]], guess[0]
 
     return None, None
 
