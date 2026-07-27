c44b33f9d7a4d41c4e33d302f9893039be8c1136
Alokchaubey123 <alokchaubey116@gmail.com>
2026-07-27 20:42:29 +0530

    Searching words Added

--- app.py
+++ app.py
@@ -4,4 +4,14 @@ file = open("environmental_thesaurus_40k.json", "r", encoding="utf-8")
 data = json.load(file)
 file.close()
 
+
+def search(word):
+    word = word.lower().strip()
+
+    if word in data:
+        return data[word], word
+
+    return None, None
+
+
 print("Environmental Thesaurus")
